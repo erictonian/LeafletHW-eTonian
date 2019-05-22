7ad1b6e690320f4ecca175862e5de622d0ef0c6c
Eric Tonian <erictonian@gmail.com>
2019-05-22 18:26:34 -0500

    part one complete

--- js/app.js
+++ js/app.js
@@ -3,16 +3,23 @@ function createFeatures(earthquakeData) {
     // Define a function we want to run once for each feature in the features array
     // Give each feature a popup describing the place and time of the earthquake
     function onEachFeature(feature, layer) {
-        layer.bindPopup("<h3>" + feature.properties.place +
-            "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+        layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>Magnitude: ${feature.properties.mag}<p>${new Date(feature.properties.time)}</p>`);
+
     }
 
     // Create a GeoJSON layer containing the features array on the earthquakeData object
     // Run the onEachFeature function once for each piece of data in the array
     const earthquakes = L.geoJSON(earthquakeData, {
-        onEachFeature: onEachFeature
-    });
-
+        onEachFeature: onEachFeature,
+        pointToLayer: (function (feature, latlng) {
+            return L.circleMarker(latlng, {
+                fillOpacity: 0.75,
+                stroke: false,
+                fillColor: circleColor(feature.properties.mag),
+                radius: circleSize(feature.properties.mag)
+            })
+        })
+    })
     // Sending our earthquakes layer to the createMap function
     createMap(earthquakes);
 }
@@ -36,8 +43,9 @@ function createMap(earthquakes) {
 
     // Define a baseMaps object to hold our base layers
     const baseMaps = {
+        "Dark Map": darkmap,
         "Street Map": streetmap,
-        "Dark Map": darkmap
+        // "Satellite": satmap
     };
 
     // Create overlay object to hold our overlay layer
@@ -46,10 +54,10 @@ function createMap(earthquakes) {
     };
 
     // Create our map, giving it the streetmap and earthquakes layers to display on load
-    const myMap = L.map("map", {
-        center: [37.09, -95.71],
-        zoom: 5,
-        layers: [streetmap, earthquakes]
+    const myMap = L.map("map-id", {
+        center: [20, 0],
+        zoom: 2,
+        layers: [darkmap, earthquakes]
     });
 
     // Create a layer control
@@ -58,11 +66,54 @@ function createMap(earthquakes) {
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(myMap);
+
+    // Create a legend to display information about our map
+    const legend = L.control({
+        position: "bottomright"
+    });
+
+    // When the layer control is added, insert a div with the class of "legend"
+    legend.onAdd = function (myMap) {
+        const div = L.DomUtil.create("div", "legend");
+        const scale = [0, 1, 2, 3, 4, 5]
+
+        // loop to create legend intervals 
+        for (let i = 0; i < scale.length; i++) {
+            div.innerHTML += '<i style="background:' + circleColor(scale[i] + 1) + '"></i> ' +
+                scale[i] + (scale[i + 1] ? '&ndash;' + scale[i + 1] + '<br>' : '+')
+        }
+        return div;
+    };
+    // Add the info legend to the map
+    legend.addTo(myMap);
+}
+
+// function to assign circle (and legend) color based on magnitude
+function circleColor(mag) {
+
+    if (mag > 5) {
+        return "#f30"
+    } else if (mag > 4) {
+        return "#f60"
+    } else if (mag > 3) {
+        return "#f90"
+    } else if (mag > 2) {
+        return "#fc0"
+    } else if (mag > 1) {
+        return "#ff0"
+    } else {
+        return "#9f3"
+    }
+}
+
+// function to assign size of circle based on magnitude
+function circleSize(mag) {
+    return mag * 2.5
 }
 
 (async function () {
-    const queryUrl = buildUrl();
+    const queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
     const data = await d3.json(queryUrl);
     // Once we get a response, send the data.features object to the createFeatures function
     createFeatures(data.features);
-})() s
+})()
---
+++ js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiZXRvbmlhbiIsImEiOiJjanZqbjdlOHcwY3ZwNDhvYXl0Z3AwNXg3In0.zo9F0BFdecFloC1b9xjPyg.NQ7yhFgYEP0fqdHkT9c6-Q";
